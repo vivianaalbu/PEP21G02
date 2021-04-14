85d705586bf75fbbad5fa721e7a5a5107ea9a654
viviana <vivianaalbu@gmail.com>
2021-04-14 21:33:00 +0300

    add module1

---
+++ modul1.py
@@ -0,0 +1,54 @@
+print('hello world')
+print('print\u200b("")')
+# print​("")
+HELLO_WORLD = 'Hello World'
+print(HELLO_WORLD)
+
+# name = 'Viviana'
+# print(3 * (name + ''))
+
+# name = 'Viviana'
+# print(name,name,name, sep='#',end='')
+
+# name='Viviana'
+# print(100*(name+'\n'))
+
+# name='Viviana \n '
+# print(100*name)
+
+
+name='Viviana'
+print(80 * '#')
+print(3 * name)
+print(name * 3)
+
+print(print)
+
+#print int
+print(3)
+print('3')
+
+print(2*(name+'\n'))
+
+print(' - ')
+
+[9: 17
+PM] Alex
+Timis(Guest)
+# printeaza un Triunghi si un Patrat
+​[9: 17
+PM] Alex
+Timis(Guest)
+
+print('Triunghi: ')
+print("   / \ ")
+print("  /   \ ")
+print(' /     \ ')
+print("/_______\ ")
+print('Patrat: ')
+print(" _____")
+print("|     |")
+print("|_____|")
+
+
+
